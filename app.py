5ec7f1927a6ffa8002b086d07a235e871adab667
chittinadas <154212417+chittinadas@users.noreply.github.com>
2025-04-30 08:10:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -56,3 +56,7 @@ def delete(filename):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+    import os
+port = int(os.environ.get('PORT', 5000))
+socketio.run(app, host='0.0.0.0', port=port)
+
